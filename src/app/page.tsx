b2732893eab13c0dc83e6d6adf54011684029f93
username <user_khirade36@gmal.com>
2024-09-23 05:36:45 +0530

    Update navbar content and css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,9 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
-      <h1>
-        Parag Page
-      </h1>
-    </div>
+    <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
+      <h1 className="text-2xl text-center">Parag Page</h1>
+    </main>
   )
     
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,55 @@
-import React from "react";
+'use client'
+import React, { useState } from "react";
+import { HoveredLink, Menu, MenuItem, ProductItem } from "./ui/navbar-menu";
+import { cn } from "@/utils/utils";
+import Link from "next/link";
 
-function Navbar(){
+
+function Navbar({ className }: { className?: string }){
+    const [active, setActive] = useState<string | null>(null);
     return(
-        <div>
-            Navbar
-        </div>
+        <div
+        className={cn("fixed top-10 inset-x-0 max-w-2xl mx-auto z-50", className)}
+        >
+            <Menu setActive={setActive}>
+                <Link href={"/"}>
+                 <MenuItem setActive={setActive } active={active}
+                  item="Home">
+                 
+                 </MenuItem>
+                </Link>
+                
+                <MenuItem setActive={setActive} active={active}
+                item="Our Courses">
+                <div className="flex flex-col space-y-4 text-sm">
+                <HoveredLink href="/cources">
+                    All Cources
+                </HoveredLink>
+                <HoveredLink href="/cources">
+                   Book Music Theory
+                </HoveredLink>
+                <HoveredLink href="/cources">
+                   Advance Composition
+                </HoveredLink>
+                <HoveredLink href="/cources">
+                   SongWritting
+                </HoveredLink>
+                <HoveredLink href="/cources">
+                   MusicProduction 
+                </HoveredLink>
+                <HoveredLink href="/cources">
+                   Music Feeling
+                </HoveredLink>
+                </div>
+                </MenuItem>
+                <Link href={"/contact"}>
+                <MenuItem setActive={setActive} active={active}
+                item="Contact Us">
+                </MenuItem>
+                </Link>
+            </Menu>
+            </div>
+        
     )
 }
 
