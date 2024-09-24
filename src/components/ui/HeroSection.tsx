e1cb2501c48a8094e45d5ca75db282e51efbdf6c
username <user_khirade36@gmal.com>
2024-09-25 02:09:41 +0530

    Add Spotlight function

--- src/components/ui/HeroSection.tsx
+++ src/components/ui/HeroSection.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import React from "react";
+import { Spotlight } from "./Spotlight";
 
 function HeroSection(){
     return(
@@ -7,13 +8,19 @@ function HeroSection(){
         className="h-auto md:h-[40rem] w-full rounded-md flex
         flex-col items-center justify-center relative overflow-hidden
         mx-auto py-10 md:py-0"
-        >
+        >    
+             <Spotlight
+             className="-top-40 left-0 md:left-60 md:-top-20"
+              fill="Yellow"
+             />
             <div className="p-4 relative z-10 w-full text-center">
                 <h1 
                  className="mt-20 md:mt-0 text-4xl md:text-7xl font-bold bg-clip-text 
                  text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400"
                 >Master the art of Music</h1>
-                <p>Dive into our comprehensive music courses and transform
+                <p
+                className="mt-4 font-normal text-base md:text-lg text-neutral-300 max-w-lg mx-auto"
+                >Dive into our comprehensive music courses and transform
                     tour musical journey today. Whether you are beginner 
                     or looking to refine your skills, Join to unnlock your
                     true potential.
