19c124bb698b61ecd2fe1630ec8273fe76d823a7
username <user_khirade36@gmal.com>
2024-09-25 02:39:36 +0530

    Update page and navbar function

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import HeroSection from "@/components/HeroSection";
-import Image from "next/image";
+
 
 export default function Home() {
   return (
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,6 @@
 'use client'
 import React, { useState } from "react";
-import { HoveredLink, Menu, MenuItem, ProductItem } from "./ui/navbar-menu";
+import { HoveredLink, Menu, MenuItem} from "./ui/navbar-menu";
 import { cn } from "@/utils/utils";
 import Link from "next/link";
 
